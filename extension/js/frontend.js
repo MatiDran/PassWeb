e56b432c8331b7d4dbb3769e1a660b2a3ba670d6
MatiDran <matimalicki99@gmail.com>
2020-11-19 13:50:46 +0100

    database

--- extension/js/frontend.js
+++ extension/js/frontend.js
@@ -1,6 +1,17 @@
 (function() {
 
 
+    var firebaseConfig = {
+        apiKey: "AIzaSyA2d9PD1WxAXcpUART0ayPegfVX7w9ay64",
+        authDomain: "keypass-56278.firebaseapp.com",
+        databaseURL: "https://keypass-56278.firebaseio.com",
+        projectId: "keypass-56278",
+        storageBucket: "keypass-56278.appspot.com",
+        messagingSenderId: "688316073383",
+        appId: "1:688316073383:web:2d2b31b2e20c4b1ea32282"
+      };
+      firebase.initializeApp(firebaseConfig);
+
     const formInstance = document.querySelector('#generator');
     const output = document.querySelector('#password');
 
@@ -32,11 +43,23 @@
         var numberOfDigits = data.numberOfDigits;
         var numberOfSpecials = data.numberOfSpecials;
 
+      
+         
+   
+       
+        
+      
+       
         if(parseInt(numberOfCapitals) + parseInt(numberOfDigits) + parseInt(numberOfSpecials) > parseInt(passLength)) {
             alert("Zbyt krótka długość hasła");
         } else {
             //output.textContent = numberOfCapitals + numberOfDigits + numberOfSpecials + passLength;
-            output.textContent = myFunction(passLength,numberOfCapitals,numberOfDigits,numberOfSpecials);
+            var passwd =  myFunction(passLength,numberOfCapitals,numberOfDigits,numberOfSpecials);
+            output.textContent = passwd
+        
+            var db = firebase.firestore();
+            db.collection("pass").doc(passwd).set({
+            })
         }
 
         e.preventDefault();
